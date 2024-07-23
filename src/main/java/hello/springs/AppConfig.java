a8c5b2581f4a93e9987a919a590742767945af89
pcochoco <cochocolatecoo@gmail.com>
2024-07-23 13:34:04 +0900

    spring container

--- src/main/java/hello/springs/AppConfig.java
+++ src/main/java/hello/springs/AppConfig.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //설정정보 configuration
+//객체의 생성과 의존관계 주입에만 관여 -> 구현체는 생성된 객체로 사용만 신경쓸 수 있음
+//DI(IoC) container = AppConfig
 @Configuration
 public class AppConfig {
     @Bean
@@ -22,17 +24,14 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository()); //MemberRepository도 AppConfig에 추가
 
         //원래는 MemberServiceImpl안에서
-        //MemberRepository memberRepository = new MemoryMemberRepository();
-        //로 만들어줌 : 구체화된 걸 넣어주는 것
-        //MemberServiceImpl에서는 MemberServiceImpl(MemberRepository ...)
-        //로 변경되도록
-        //MemberRepository memberRepository만 부르면서 MemoryMemberRepository로
-        //만들어줌
+        //MemberRepository memberRepository = new MemoryMemberRepository(); 로 만들어줌 : 구체화된 걸 넣어주는 것
+        //MemberServiceImpl에서는 MemberServiceImpl(MemberRepository ...) 로 변경되도록
+        //MemberRepository memberRepository만 부르면서 MemoryMemberRepository로 만들어줌
         //MemberServiceImpl은 인터페이스에만 의존
         //AppConfig에서 구체화를 맡음
     }
 
-    @Bean
+    @Bean //@ComponentScan 프로젝트 최상단, 각 repository, class 별 annotation에 따라 생략 가능
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
 
--- src/main/java/hello/springs/member/MemberServiceImpl.java
+++ src/main/java/hello/springs/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.springs.member;
 //구현체가 하나인 경우 Impl을 붙이는 관례
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository; //생성자 주입용 private, final -> 불변
     // = new MemoryMemberRepository();
     //ctrl shift enter : 세미콜론까지 자동완성
+
+    //생성자 주입 : 1번만 초기화
     public MemberServiceImpl(MemberRepository memberRepository){
+        //AppConfig / SpringContainer에 의한 의존관계 주입 = DI = 동적 객체 의존관계 (runtime에 어떤 객체가 사용되는지 알 수 있음)
+        //의존관계는 정적, 동적인 경우로 분리해 존재
+        //IoC : 제어의 역전
+
         this.memberRepository = memberRepository;
     }
     @Override
